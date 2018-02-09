731a36bfdd1e855f6a43752454dd5f8577b1dbfe
alexmejor <alexmejor_5@hotmail.com>
2018-02-09 04:32:27 +0100

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -1,60 +1,146 @@
 $(document).ready(function () {
+    $("#rocrank").hide();
+    $("#tftrank").hide();
+
     $("#name").focus();
-    $("#newuser").click(function() {
-        $(".principal").html("<img src='http://rocarena.com/np/modules/Upload/uploads/59c3c0348520ess.png'>");
-        $(".principal").css("background-color","black");
-    });
-    
-    $("#boton").click(function () {
+    $("#newuser").click(function () {
         $.ajax({
-            url: "http://rocarena.com/np/mobile/login.php",
+            url: "http://alexdaw.ddns.net/ajax/warcraft/php/login.php",
+            type: "POST",
+            dataType: "json",
+            data: {
+                username: $("#name").val(),
+                newuser: $("#newuser").val(),
+                password: $("#password").val()
+            },
+            success: function (response) {
+                if (response[0].error == "false") {
+                    $("#info").html("<p style='color:green'>The user has been created!</p>");
+                } else if (response[0].error == "empty") {
+                    $("#info").html("<p style='color:red'>Please fill the fields</p>");
+                } else if (response[0].error == "true") {
+                    $("#info").html("<p style='color:red'>This name is already taken</p>");
+                }
+            }
+        });
+    });
+
+    $("#logon").click(function () {
+        $("#info").html("");
+        $.ajax({
+            url: "http://alexdaw.ddns.net/ajax/warcraft/php/login.php",
             type: "POST",
             data: {
                 username: $("#name").val(),
+                login: $("#logon").val(),
                 password: $("#password").val()
             },
-            dataType: "json",
-            success: function (respuesta) {
-                if (respuesta.error) {
+            success: function (response) {
+                if (response[0].error == "false") {
+                    $("#login").html('');
+                    $(".wc3logo").css("margin-bottom", "25px");
+                    $("#rocrank").show();
+                    $("#tftrank").show();
+                    $("#rocrank").click(function () {
+                        var xmlhttp = new XMLHttpRequest();
+
+                        var informacionUsuario = "&bnetrocranking=rank";
+                        xmlhttp.onreadystatechange = function () {
+                            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                                //var datos = JSON.parse(xmlhttp.responseText);
+                                var mensaje = xmlhttp.responseText;
+                                var filaCampos = "<tr><td>Rank</td><td>Lvl</td><td>Clan</td><td>Race</td><td>Player</td></tr>";
+                                $("#login").html(mensaje);
+                                $("table").eq(0).prepend(filaCampos);
+                                $(".principal").css("font-family", "Roboto");
+                                $(".principal").css("padding", '10px');
+                            }
+                        }
+                        xmlhttp.open("POST", "http://alexdaw.ddns.net/ajax/warcraft/php/classicbnet.php", true);
+                        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                        xmlhttp.send(informacionUsuario);
+                    });
+                    $("#tftrank").click(function () {
+                        var xmlhttp = new XMLHttpRequest();
+
+                        var informacionUsuario = "&bnettftranking=rank";
+                        xmlhttp.onreadystatechange = function () {
+                            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+                                //var datos = JSON.parse(xmlhttp.responseText);
+                                var mensaje = xmlhttp.responseText;
+                                $("#login").html(mensaje);
+                                var filaCampos = "<tr><td>Rank</td><td>Lvl</td><td>Clan</td><td>Race</td><td>Player</td></tr>";
+                                $("table").eq(0).prepend(filaCampos);
+                                $(".principal").css("font-family", "Roboto");
+                                $(".principal").css("padding", '10px');
+                            }
+                        }
+                        xmlhttp.open("POST", "http://alexdaw.ddns.net/ajax/warcraft/php/classicbnet.php", true);
+                        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                        xmlhttp.send(informacionUsuario);
+                    });
+                } else if (response[0].error == "true") {
                     $("#info").html("<p style='color:red'>Incorrect Password</p>");
-                } else {
-                    $(".principal").html('<input type="button" class="button" id="top20" value="Top 20"><br><br>');
-                    $(".principal").append("<div class='ranking'></div>");
-                    $(".wc3logo").css("margin-bottom","25px");
                 }
-                $("#top20").click(function () {
-                    var xmlhttp = new XMLHttpRequest();
-                    
-                    var informacionUsuario = "&rank=rank";
-                    xmlhttp.onreadystatechange = function() {
-                    if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                        //var datos = JSON.parse(xmlhttp.responseText);
-                        var mensaje =  xmlhttp.responseText;
-                        $(".ranking").html(mensaje).slideToggle();
-                        var filaCampos = "<tr><td>Rank</td><td>Lvl</td><td>Clan</td><td>Race</td><td>Player</td></tr>";
-                        $("table").eq(0).prepend(filaCampos);
-                        $(".ranking").css("padding",0);
-                        $(".principal").css("font-family","Roboto");
-                    }
-                }
-                xmlhttp.open("POST", "http://alexdaw.ddns.net/ajax/warcraft/classicbnet.php" , true);
-                xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-                xmlhttp.send(informacionUsuario);
-//                    $.ajax({
-//                        url: "classicbnet.php",
-//                        type: "GET",
-//                        data: {
-//                            rank: ""
-//                        },
-//                        dataType: "text",
-//                        success: function (respuesta) {
-//                            $(".principal").html(respuesta);
-//                            $(".principal").css("padding",0);
-//                        }
-//                    });
-                });
+
             }
         });
+        //ROCARENA LOGIN
+        //        $.ajax({
+        //            url: "http://rocarena.com/np/mobile/login.php",
+        //            type: "POST",
+        //            data: {
+        //                username: $("#name").val(),
+        //                password: $("#password").val()
+        //            },
+        //            dataType: "json",
+        //            success: function (respuesta) {
+        //                if (respuesta.error) {
+        //                } else {
+        //                    
+        //                }
+        //            }
+        //        });
 
     });
 });
+
+function bnetRocRanking() {
+    var xmlhttp = new XMLHttpRequest();
+
+    var informacionUsuario = "&bnetrocranking=rank";
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            //var datos = JSON.parse(xmlhttp.responseText);
+            var mensaje = xmlhttp.responseText;
+            var filaCampos = "<tr><td>Rank</td><td>Lvl</td><td>Clan</td><td>Race</td><td>Player</td></tr>";
+            $("#login").html(mensaje);
+            $("table").eq(0).prepend(filaCampos);
+            $(".principal").css("font-family", "Roboto");
+            $(".principal").css("padding", '10px');
+        }
+    }
+    xmlhttp.open("POST", "http://alexdaw.ddns.net/ajax/warcraft/php/classicbnet.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(informacionUsuario);
+}
+
+function bnetTftRanking() {
+    var xmlhttp = new XMLHttpRequest();
+
+    var informacionUsuario = "&bnettftranking=rank";
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            //var datos = JSON.parse(xmlhttp.responseText);
+            var mensaje = xmlhttp.responseText;
+            $("#login").html(mensaje);
+            var filaCampos = "<tr><td>Rank</td><td>Lvl</td><td>Clan</td><td>Race</td><td>Player</td></tr>";
+            $("table").eq(0).prepend(filaCampos);
+            $(".principal").css("font-family", "Roboto");
+            $(".principal").css("padding", '10px');
+        }
+    }
+    xmlhttp.open("POST", "http://alexdaw.ddns.net/ajax/warcraft/php/classicbnet.php", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send(informacionUsuario);
+}
